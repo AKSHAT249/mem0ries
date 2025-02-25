7c156f649bf7b0598226bedce12277ca5d701353
AKSHAT249 <akshs2937@gmail.com>
2025-02-26 01:52:29 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
-
+dotenv.config();
 const app = express();
+
 
 
 
@@ -18,12 +19,14 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 
-const CONNECTION_URL='mongodb+srv://akshs2937:O3LjaG9tRjC71pB6@cluster0.3afys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL=process.env.CONNECTION_URL;
+
+const PORT = process.env.PORT;
+
 
 // , {useNewUrlParser:true, useUnifiedTopology:true}
 mongoose.connect(CONNECTION_URL)
-.then( () => { app.listen(5000, () => {console.log(`Server running on port ${PORT}`)}) } )
+.then( () => { app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)}) } )
 .catch( (error) => {
     console.log("errrr",error.message)
 } );
